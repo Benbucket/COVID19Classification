fe577a9a37ce14e8478157f40b4bf83a6f5a1495
unknown <tangwoyih@gmail.com>
2022-03-10 13:53:42 +0800

    changed \ to /

--- helper.py
+++ helper.py
@@ -1,10 +1,10 @@
 import os
 
 path = os.path.abspath(os.getcwd())
-output_path = r'yolov5\runs\detect\exp'
+output_path = 'yolov5/runs/detect/exp/'
 
 def predictor(img):
     source = os.path.join('yolov5/images', img)
     os.system(f'python yolov5/detect.py --weights yolov5/best.pt --source {source}')
-    output_img = os.path.join(path, output_path) + f'\\{img}'
+    output_img = os.path.join(path, output_path) + img
     return output_img
