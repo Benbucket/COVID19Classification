bfaaaf0ca473a8bb4e3fd2f3070b27ec56aeefb5
unknown <tangwoyih@gmail.com>
2022-03-10 13:56:23 +0800

    update: change line 8 source to img

--- helper.py
+++ helper.py
@@ -5,6 +5,6 @@ output_path = 'yolov5/runs/detect/exp/'
 
 def predictor(img):
     source = os.path.join('yolov5/images', img)
-    os.system(f'python yolov5/detect.py --weights yolov5/best.pt --source {source}')
+    os.system(f'python yolov5/detect.py --weights yolov5/best.pt --source {img}')
     output_img = os.path.join(path, output_path) + img
     return output_img
