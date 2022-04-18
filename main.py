9bf56b9a84203d215a946f9302e27d21f33fb56e
unknown <tangwoyih@gmail.com>
2022-04-18 17:03:46 +0800

    Added: customBest.pt

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import torch
 import time
 import os
 from PIL import Image
-st.title('COVID-19 Detector')
 
+
+st.title('COVID-19 Detector')
+with st.expander("How to use?"):
+    st.write("1. Upload a Chest X-Ray image in .jpg format.")
+    st.write("2. View the results below.")
+    st.write("Note: The table below the images shows the information of the detected opacities.")
+
+# File uploader
 uploaded_file = st.file_uploader("Upload Image", type='jpg')
 
 
 def predictor(img):
     # Model
     torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path='bestModel.pt')
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path='customBest.pt')
     # Inference
     results = model(img)
     results.pandas().xyxy[0]
